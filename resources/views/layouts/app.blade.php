bdf48710ade1e4bfaa66cde65e7c852159e7a01c
Zouhayr-BAHOUMI <bahoumizouhayr@gmail.com>
2025-04-28 19:08:49 +0100

    update front landing page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,9 @@
 </head>
 <body>
     @yield('content')
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     @stack('scripts')
 </body>
 </html>
